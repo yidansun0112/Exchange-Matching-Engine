e750e1fcdfcd5af627a6e6345fcbb53fb22fefd5
yidansun0112 <ys303@duke.edu>
2021-03-26 13:30:13 +0800

    write main

---
+++ server.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <pqxx/pqxx>
+
+using namespace std;
+using namespace pqxx;
+
+int main (int argc, char *argv[]) 
+{
+
+  //Allocate & initialize a Postgres connection object
+  connection *C;
+
+  try{
+    //Establish a connection to the database
+    //Parameters: database name, user name, user password
+    C = new connection("dbname=EXCHANGE user=postgres password=passw0rd");
+    if (C->is_open()) {
+      cout << "Opened database successfully: " << C->dbname() << endl;
+    } else {
+      cout << "Can't open database" << endl;
+      return 1;
+    }
+  } catch (const std::exception &e){
+    cerr << e.what() << std::endl;
+    return 1;
+  }
