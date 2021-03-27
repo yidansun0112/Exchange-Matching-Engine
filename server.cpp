302228013bd064810edfed0125c5388be7c1d624
yidansun0112 <ys303@duke.edu>
2021-03-27 09:01:36 -0400

    edit server

--- server.cpp
+++ server.cpp
@@ -25,10 +25,10 @@ int main (int argc, char *argv[])
   string msg2=db.createSymbol("abc",0,-100);
   cout<<msg2<<endl;
 
-  string msg3=db.createOrder("a",-50,100,0);
+  string msg3=db.createOrder("abc",-50,100,0);
   cout<<msg3<<endl;
 
-  string msg4=db.createOrder("abc",50,100,5);
+  string msg4=db.createOrder("abc",50,100,0);
   cout<<"id 5"<<msg4<<endl;
 
   db.disconnect();
