199349951a2575d21bc1c3021ae1f50bca1e866b
ys303 <ys303@duke.edu>
2021-03-30 02:38:21 -0400

    test.sh 28

--- docker-deploy/src/test.sh
+++ docker-deploy/src/test.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
-./client 127.0.0.1 create.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 1000.txt 1
-./client 127.0.0.1 cancel.txt 1
+./client 127.0.0.1 create.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 1000.txt
+./client 127.0.0.1 cancel.txt
