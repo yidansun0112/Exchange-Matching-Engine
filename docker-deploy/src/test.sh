a37dd0f4b407679f4468a77ccc133aa1fb17749d
ys303 <ys303@duke.edu>
2021-03-30 08:31:23 -0400

    create query.txt

--- docker-deploy/src/test.sh
+++ docker-deploy/src/test.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 ./client 127.0.0.1 create.txt
-./client 127.0.0.1 1000.txt &
-./client 127.0.0.1 1000.txt &
-./client 127.0.0.1 1000.txt &
-./client 127.0.0.1 1000.txt
+./client 127.0.0.1 query.txt &
+./client 127.0.0.1 query.txt &
+./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+# ./client 127.0.0.1 query.txt &
+./client 127.0.0.1 query.txt
 wait
 ./client 127.0.0.1 cancel.txt
